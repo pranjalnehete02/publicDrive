4ef1be4776ae7c3b68a88aff1c33da8df086b41e
Pranjal Nehete <pranjalnehete002@gmail.com>
2025-01-30 02:55:50 +0530

    Registration and Login page

---
+++ enterprise-frontend/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Login />} /> {/* Default to login */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
