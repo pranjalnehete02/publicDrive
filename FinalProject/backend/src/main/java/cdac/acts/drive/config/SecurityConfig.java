bb1ad0f39d85fece194a12fb6020bcb0c82ffa75
AdityaNikhate <nikhateaditya123@gmail.com>
2025-02-05 18:21:45 +0530

    done

--- FinalProject/backend/src/main/java/cdac/acts/drive/config/SecurityConfig.java
+++ FinalProject/backend/src/main/java/cdac/acts/drive/config/SecurityConfig.java
@@ -44,11 +44,4 @@ public class SecurityConfig {
     }
 
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return web -> web.ignoring().requestMatchers(
-                "/swagger-ui/**", "/v3/api-docs/**"
-        );
-    }
-
 }
